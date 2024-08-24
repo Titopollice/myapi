29b5111c671fa12bf57765d40c4bbe78479197e9
Tiago Oliveira <92068949+Titopollice@users.noreply.github.com>
2024-08-23 22:49:18 -0300

    feat: ajuste no formato de conexão do banco

--- db.js
+++ db.js
@@ -1,18 +1,19 @@
-// db.js
-const mysql = require("mysql2");
+require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
+const mysql = require('mysql2');
 
+// Configuração da conexão com o banco de dados usando variáveis de ambiente
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "db_adegas",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("Erro ao conectar ao banco de dados:", err);
+    console.error('Erro ao conectar ao banco de dados:', err);
   } else {
-    console.log("Conectado ao banco de dados MySQL.");
+    console.log('Conectado ao banco de dados MySQL.');
   }
 });
 

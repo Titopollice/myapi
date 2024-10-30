66eb18a9dc0f619ded508a5e2b089838c598f50f
Tiago Oliveira <92068949+Titopollice@users.noreply.github.com>
2024-10-30 14:27:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,6 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });

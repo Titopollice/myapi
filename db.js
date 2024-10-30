bc3da63005380f3e331dc51258f7046b9ae15a76
Tiago Oliveira <92068949+Titopollice@users.noreply.github.com>
2024-10-30 18:24:14 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,19 +1,14 @@
-require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
-const mysql = require('mysql2');
+require("dotenv").config(); // Carrega as variáveis de ambiente do arquivo .env
+const mysql = require("mysql2");
 
-// Configuração da conexão com o banco de dados usando variáveis de ambiente
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+// Configuração da conexão usando a URL única
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect((err) => {
   if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
+    console.error("Erro ao conectar ao banco de dados:", err);
   } else {
-    console.log('Conectado ao banco de dados MySQL.');
+    console.log("Conectado ao banco de dados MySQL.");
   }
 });
 
